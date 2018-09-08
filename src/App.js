5cfb0a7df574493750b9dad99d2a0f4a56aacd85
Julius Suominen <julius.suominen@gmail.com>
2018-09-08 18:32:39 +0200

    at video 114

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAcsdY5wNsrziALfgKyePRTqj8ZHGOQ0sM',
+      authDomain: 'manager-3b7c8.firebaseapp.com',
+      databaseURL: 'https://manager-3b7c8.firebaseio.com',
+      projectId: 'manager-3b7c8',
+      storageBucket: 'manager-3b7c8.appspot.com',
+      messagingSenderId: '604551993612'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
