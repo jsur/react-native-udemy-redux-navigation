5d6739a1b3f757b6611edbac30c3044707057fc1
Julius Suominen <julius.suominen@gmail.com>
2018-09-08 20:23:10 +0200

    at video 116

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -17,6 +17,7 @@ class LoginForm extends Component {
             label="Email"
             placeholder="email@gmail.com"
             onChangeText={this.onEmailChange}
+            value={this.props.email}
           />
         </CardSection>
         <CardSection>
@@ -37,4 +38,10 @@ class LoginForm extends Component {
   }
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const mapStateToProps = (state) => {
+  return {
+    email: state.auth.email
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
