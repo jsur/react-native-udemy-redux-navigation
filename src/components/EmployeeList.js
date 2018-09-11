abbe7f02921d1c367875c4a657ac2152b22e0c04
Julius Suominen <julius.suominen@gmail.com>
2018-09-11 15:56:09 +0200

    at video 157

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
+
+  componentWillMount() {
+    this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps is the next set of props to the component
+    // old props can be access still with this.props
+    this.createDataSource(nextProps);
+  }
+
+  // To avoid creating ListView's datasource only once:
+    // 1. this is used when component is mounted
+    // 2. and also when component just gets new props
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
+
   render() {
+    console.log(this.props);
     return (
-      <View>
-        <Text>EmployeeList</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
---
+++ src/components/ListItem.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { CardSection } from './common';
+
+class ListItem extends Component {
+  onRowPress = () => {
+    // router-flux allows a navigation which passes additional props 
+    // to the component that will be rendered
+    Actions.employeeEdit({ employee: this.props.employee });
+  }
+
+  render() {
+    const { name } = this.props.employee;
+
+    return (
+      <TouchableWithoutFeedback onPress={this.onRowPress}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              {name}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
+
+export default ListItem;
